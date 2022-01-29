cd828af3229955e1f9cb8f8838f88be2902f0b97
Doudii <doudydax@gmail.com>
2022-01-29 02:50:34 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../styles/App.css';
+import Header from'./header';
+//import Body from './body';
+
+const App = () => {
+
+  return (
+    <div >
+      <Header 
+      />
+      {/* <Body />         */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import '../styles/Header.css'
+//import logo from '../assets/logo.png'
+
+function Header() {
+    //const title = 'Présentation vidéo'
+    return(
+    <div className='container'>    
+        <div className="testcss">
+            EwenFlix
+        </div>
+        <div className='centerHead'>
+            <a className='buttonHeader'>
+                Je témoigne sur ma maladie
+            </a>
+        </div>
+    </div>    
+    )
+}
+
+export default Header
